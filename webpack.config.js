c3f8c24e9d30e1e11595a601df88e523cd002c50
Sultana <rsultan@apac.corpdir.net>
2022-08-22 11:24:55 +0530

    json loader added

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
+const json = require('./data.json');
+
 module.exports = {
     entry: "./src/controller.js",
     output: {
         filename: "controller.bundle.js",
         publicPath: "/webApp_SmartCity/",
+    },
+    module: {
+        rules: [{
+            test: /\.json$/,
+            use: 'json-loader'
+        }]
     }
 }
